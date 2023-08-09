6ba4049d235ae0bdfedd1bc6e1ce7ccdd85893ab
Alex I <ailushyk@gmail.com>
2023-08-09 18:51:18 +0200

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    'turbo/no-undeclared-env-vars': 'off',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'never' },
+    ],
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
+}
